a2bbd69237d9c896e7742f992b2cc3dad10c3d46
Fede <federico.cabralet32@gmail.com>
2026-03-26 11:53:26 -0300

    Add files via upload

---
+++ Trabajo Practico N°3.cs
@@ -0,0 +1,43 @@
+﻿string Ejercicio1(int Num){
+    string res;
+    if (Num > 0)
+    {
+        res = "El número es positivo.";
+        return res;
+    }
+    else if (Num < 0)
+    {
+        res=("El número es negativo.");
+        return res;
+    }
+    else
+    {
+        res=("El número es cero.");
+        return res;
+    }
+}
+Console.WriteLine(Ejercicio1(Convert.ToInt32(Console.ReadLine())));
+
+string Ejercicio2(int edad) {
+    string edadtotal;
+    if (edad >= 18)
+    {
+        edadtotal = "¡Bienvenido a la fiesta!";
+        return edadtotal;
+    }
+    else
+    {
+        edadtotal = "Lo siento, eres muy joven.";
+        return edadtotal;
+    }
+}
+Console.WriteLine(Ejercicio2(Convert.ToInt32(Console.ReadLine())));
+
+string Ejercicio3(string contra) {
+    string mensaje;
+    if (contra = "python123")
+    {
+        mensaje = "¡Contraseña Correcta! Acceso concedido.";
+        return mensaje;
+    
+    }
