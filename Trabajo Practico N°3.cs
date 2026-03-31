912f33d1675b2cc598b61dc7b4c9ba630aa5c005
Fede <federico.cabralet32@gmail.com>
2026-03-31 11:24:23 -0300

    EJ4 Y EJ5

--- Trabajo Practico N°3.cs
+++ Trabajo Practico N°3.cs
@@ -33,7 +33,8 @@ string Ejercicio2(int edad) {
 }
 Console.WriteLine(Ejercicio2(Convert.ToInt32(Console.ReadLine())));
 
-string Ejercicio3(string contra) {
+string Ejercicio3(string contra)
+{
     string mensaje;
     if (contra = "python123")
     {
@@ -41,3 +42,45 @@ string Ejercicio3(string contra) {
         return mensaje;
     
     }
+    else
+    {
+        mensaje = "¡Contraseña Incorrecta! Autodestrucción en 5 minutos!";
+        return mensaje;
+    }
+}
+Console.WriteLine(Ejercicio3(Console.ReadLine()));
+
+string Ejercicio4(int numer)
+{
+    string res;
+    if (numer % 2==0)
+    {
+        res = "El numero es par.";
+        return res;
+
+    }
+    else
+    {
+        res = "El numero es impar.";
+        return res;
+    }
+}
+Console.WriteLine(Ejercicio4(Convert.ToInt32(Console.ReadLine())));
+
+string Ejercicio5(int edad)
+{
+    string res;
+    Console.WriteLine("Compra pochoclos?");
+    string pochoc = Console.ReadLine();
+    if (edad >= 65 && pochoc == "si")
+    {
+        res = ("Felicidades! Tiene entrada gratuita al cine.");
+        return res;
+    }
+    else
+    {
+        res = ("Compra la entrada o raja de aca.");
+        return res;
+    }
+}
+Console.WriteLine(Ejercicio5(Convert.ToInt32(Console.ReadLine())));
