8f68183f3490b646086fd0cd99b7dd549113ded5
Fede <federico.cabralet32@gmail.com>
2026-09-01 11:37:56 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<Ubicacion> historialUbicaciones = new Stack<Ubicacion>();
+
+            Ubicacion PrimerUbicacion = new Ubicacion(6, -43, "Cueva");
+            Ubicacion SegundaUbicacion = new Ubicacion(125, 864, "Desierto");
+            Ubicacion TerceraUbicacion = new Ubicacion(333, -333, "Bosque");
+
+            historialUbicaciones.Push(PrimerUbicacion);
+            historialUbicaciones.Push(SegundaUbicacion);
+            historialUbicaciones.Push(TerceraUbicacion);
+
+            foreach (Ubicacion Recorrer in historialUbicaciones)
+            {
+                Console.WriteLine($"Coordenada X:  {Recorrer.X}  Coordenada Y:  {Recorrer.Y}  Zona:  {Recorrer.NombreZona}");
+            }
+
+
+           Ubicacion test = historialUbicaciones.Pop();
+
+            Console.WriteLine($"Ubicacion Removida = Coordenada X:  {test.X}  Coordenada Y:  {test.Y}  Zona:  {test.NombreZona}");
+
+            Ubicacion test2 = historialUbicaciones.Peek();
+
+            Console.WriteLine($"Ubicacion Actual = Coordenada X:  {test2.X}  Coordenada Y:  {test2.Y}  Zona:  {test2.NombreZona}");
+
+        }
+    }
+}
---
+++ Ubicacion.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public struct Ubicacion
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public string NombreZona { get; set; }
+
+    public Ubicacion(int X, int Y, string NombreZona)
+    {
+            this.X = X;
+            this.Y = Y;
+            this.NombreZona = NombreZona;
+    }
+
+    }
+}
