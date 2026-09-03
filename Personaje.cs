7870c7d99dfe072018f3dd8776207ad8641721c2
Fede <federico.cabralet32@gmail.com>
2026-09-03 09:47:41 -0300

    Add files via upload

---
+++ Personaje.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evaluacion_Structs_Y_Pilas
+{
+    public struct Personaje
+    {
+
+        public int VidaTotal { get; set; }
+        public int VidaActual { get; set; }
+        public string UltimaAccion { get; set; }
+
+
+       public Personaje (int VidaTotal, int VidaActual, string UltimaAccion)
+       {
+            this.VidaActual = VidaActual;
+            this.VidaTotal = VidaTotal;
+            this.UltimaAccion = UltimaAccion;
+       }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Reflection.Metadata;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Evaluacion_Structs_Y_Pilas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int DañoGolpe = 20;
+
+            Stack<Personaje> HistorialDelPersonaje = new Stack<Personaje>();
+
+
+            Personaje Historial1 = new Personaje (100, 84, "Correr");
+            HistorialDelPersonaje.Push(Historial1);
+
+            Personaje Historial2 = new Personaje (100, 85, "Curar");
+            HistorialDelPersonaje.Push(Historial2);
+
+            Personaje Historial3 = new Personaje (100, 95, "Bailar");
+            HistorialDelPersonaje.Push(Historial3);
+
+
+
+            foreach (Personaje Recorrer in HistorialDelPersonaje)
+            {
+                Console.WriteLine($"VidaActual: {Recorrer.VidaActual} VidaTotal: {Recorrer.VidaTotal} Ultima Accion: {Recorrer.UltimaAccion}");
+            }
+
+
+            Personaje VolverEnElTiempo = HistorialDelPersonaje.Pop();
+            Console.WriteLine($"Ultima Accion Borrada: {VolverEnElTiempo.UltimaAccion}");
+
+
+            Personaje HistorialDaño = new Personaje(100, (VolverEnElTiempo.VidaActual - DañoGolpe), "Golpe Recibido");
+            HistorialDelPersonaje.Push(HistorialDaño);
+
+            foreach (Personaje Recorrer2 in HistorialDelPersonaje)
+            {
+                Console.WriteLine($"VidaActual: {Recorrer2.VidaActual} VidaTotal: {Recorrer2.VidaTotal} Ultima Accion: {Recorrer2.UltimaAccion}");
+            }
+
+
+        }
+    }
+}
