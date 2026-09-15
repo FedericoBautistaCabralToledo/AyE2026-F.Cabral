087e2c0743fe8afd9a59aa2fec277ba814f869d2
Fede <federico.cabralet32@gmail.com>
2026-09-15 11:11:29 -0300

    Update AccionTexto.cs

--- AccionTexto.cs
+++ AccionTexto.cs
@@ -1,12 +1,24 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp2
+namespace ConsoleApp1
 {
     internal class AccionTexto
     {
+
+        public string TipoAccion { get; }
+        public string Contenido { get; }
+        public int FechaHora { get; }
+
+        public AccionTexto(string tipoAccion, string contenido)
+        {
+            TipoAccion = tipoAccion;
+            Contenido = contenido;
+            FechaHora = FechaHora;
+        }
+
     }
 }
