f8b312c3a9170250312465d2f041c677550b2e0c
Fede <federico.cabralet32@gmail.com>
2026-09-17 11:58:58 -0300

    Add files via upload

---
+++ AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace EjercicioBaseDeDatos
+{
+    internal class AppDbContext : DbContext  
+    {
+        public DbSet<Punto2D> Punto2D => Set<Punto2D>();
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) 
+        {
+
+            string ConnectionString = "Server=localhost;Port=3307;Database == tp_bdd;Uid=root;pwd=;"; 
+            var serverVersion = ServerVersion.AutoDetect(ConnectionString); 
+            optionsBuilder.UseMySql(ConnectionString, serverVersion);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Security.Permissions;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace EjercicioBaseDeDatos
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using var db = new AppDbContext();
+
+            var LoQueVoyAInsertar = new Punto2D(2, 4);
+
+            db.Punto2D.Add(LoQueVoyAInsertar);
+            db.SaveChanges();
+
+            void Mostrar()
+            {
+                Console.WriteLine(LoQueVoyAInsertar);
+            }
+            Mostrar();
+        }
+    }
+}
