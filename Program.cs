9442132f8d85fdd46ce15e986e34682f11f8ae00
Fede <federico.cabralet32@gmail.com>
2026-09-17 11:59:45 -0300

    Enhance Program.cs with database operations
    Added database creation and data manipulation logic.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System.Security.Permissions;
+using System.Security.Permissions;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 
@@ -12,14 +12,42 @@ namespace EjercicioBaseDeDatos
 
             var LoQueVoyAInsertar = new Punto2D(2, 4);
 
-            db.Punto2D.Add(LoQueVoyAInsertar);
-            db.SaveChanges();
 
             void Mostrar()
             {
                 Console.WriteLine(LoQueVoyAInsertar);
             }
             Mostrar();
+
+            string SQL = "CREATE DATABASE tp_bdd; USE tp_bdd; CREATE TABLE Puntos( id int PRIMARY KEY AUTO_INCREMENT, int X, int Y); ";
+
+
+            db.Punto2D.Add(LoQueVoyAInsertar);
+            db.SaveChanges();
+
+            int id = 3;
+            var datoBuscado = LoQueVoyAInsertar;
+            datoBuscado = await db.Punto2D.FindAsync(id);
+
+
+            if (datoBuscado != null)
+            {
+                int XUsuario = Console.ReadLine();
+                int YUsuario = Console.ReadLine();
+                datoBuscado.X = XUsuario;
+                datoBuscado.Y = YUsuario;
+
+            }
+            int id2 = 1;
+            var datoBuscado2 = 1;
+            datoBuscado2 = await db.Punto2D.FindAsync(id);
+
+            if (datoBuscado != null)
+            {
+                db.Punto2D.Remove(datoBuscado);
+                await db.SaveChangesAsync();
+            }
+
         }
     }
 }
